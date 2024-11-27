7b9ec84dd4f1cf2b3241c04a98c136f92c42ec32
tur38841 <tur38841@temple.edu>
2024-11-27 13:00:45 -0500

    Add files via upload

--- finalproject.py
+++ finalproject.py
@@ -1,14 +1,17 @@
 import pygame
 from pygame.locals import * 
 
-pygame.init()
+pygame.init() #initialize pygame
 
-screen_width = 864
-screen_height= 936
+screen_width = 800
+screen_height= 800
+
+#frames per second
+fps = pygame.time.Clock()
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-pygame.display.set_caption("Meteors Incoming!")
+pygame.display.set_caption("Stars Incoming!")
 
 
 
@@ -16,11 +19,43 @@ bg = pygame.image.load('img/hold.jpg')
 bg = pygame.transform.scale(bg, (screen_width, screen_height))  # Scale it to fit the screen size
 
 
+#example
+class ship(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        super().__init__()
+        self.image = pygame.image.load('img/ship.png') #this is the specific image
+        self.image = pygame.transform.scale(self.image, (100,100))
+        self.rect = self.image.get_rect()#this will create the boundaries of the image for youh
+        self.rect.center = (x,y)
+
+    def move(self):
+        key_pressed = pygame.key.get_pressed()
+
+
+        #this allows us to stay on the screen
+        if key_pressed[K_UP] and self.rect.top>0:
+            self.rect.move_ip(0, -10)
+
+        if key_pressed[K_DOWN] and self.rect.bottom < screen_height:
+            self.rect.move_ip(0,10)
+
+        #7 is the speed
+
+
+        
+
+ship = ship(screen_width // 4, screen_height // 2)
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(ship)
+
+
 run = True
 while run: 
     
     screen.blit(bg, (0,0))
-    
+    ship.move()
+    all_sprites.draw(screen)
     
 
     for event in pygame.event.get():
@@ -28,7 +63,8 @@ while run:
             run = False
             
     pygame.display.update()
-    
+    fps.tick(60)
+
 
 
 
